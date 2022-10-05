581cdc4d5d37bc39e4e9902bafed79b2ddecaaf5
DawidC4 <113301335+DawidC4@users.noreply.github.com>
2022-10-05 09:48:08 +0200

    Create zad4.sh

---
+++ bash/zad4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Pociag kolei mazowieckich ze stacji zad4.sh do stacji https://github.com/DawidC4/bash/ w dniu dzisiejszym nie kursuje."
+echo "Za utrudnienia serdecznie przepraszamy i życzymy miłej podróży :)"
